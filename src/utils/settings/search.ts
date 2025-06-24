040e289dcf32abb30030100495c97bf51b5e1644
luode <luode@ellipalwallet.com>
2025-06-24 15:31:44 +0800

    Google

--- src/utils/settings/search.ts
+++ src/utils/settings/search.ts
@@ -3,6 +3,11 @@ import { SettingItem } from '@/types'
 
 const searchList: SettingItemChildren<Search> = [
   {
+    name: '谷歌',
+    enName: 'Google',
+    value: { name: '谷歌', enName: 'Google', url: 'https://www.google.com/search', key: 'q', favicon: '/svg/google.svg', s: 'gg' },
+  },
+  {
     name: '必应',
     enName: 'Bing',
     value: { name: '必应', enName: 'Bing', url: 'https://www.bing.com/search', key: 'q', favicon: '/svg/bing.svg', s: 'bi' },
@@ -11,11 +16,6 @@ const searchList: SettingItemChildren<Search> = [
     name: '百度',
     enName: 'Baidu',
     value: { name: '百度', enName: 'Baidu', url: 'https://www.baidu.com/s', key: 'wd', favicon: '/svg/baidu.svg', s: 'bd' },
-  },
-  {
-    name: '谷歌',
-    enName: 'Google',
-    value: { name: '谷歌', enName: 'Google', url: 'https://www.google.com/search', key: 'q', favicon: '/svg/google.svg', s: 'gg' },
   },
   {
     name: '搜狗',
